9088f03a413d41f8f8aeb4555f91b78c3ab69a90
Saeed <saeedworkemail2020@gmail.com>
2026-05-13 15:54:01 +0330

    edite

---
+++ lib/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Home Page')),
+        body: const Center(child: Text('Welcome to the Home Page!')),
+      ),
+    );
+  }
+}
