6732cd8af093bbf45a9c0ece53468fb64d40852f
Saeed <saeedworkemail2020@gmail.com>
2026-06-05 19:24:01 +0400

    Test: exchange data

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:for_connection/home.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _MyAppState extends State<MyApp> {
+  final channel = WebSocketChannel.connect(Uri.parse('ws://192.168.4.1:81'));
+
+  String text = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    channel.stream.listen((message) {
+      setState(() {
+        text += "\n$message";
+      });
+    });
+  }
+
+  void send(String msg) {
+    channel.sink.add(msg);
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const Homepage(),
+      home: Scaffold(
+        appBar: AppBar(title: Text("ESP32 WebSocket")),
+        body: Column(
+          children: [
+            ElevatedButton(onPressed: () => send("on"), child: Text("LED ON")),
+            ElevatedButton(
+              onPressed: () => send("off"),
+              child: Text("LED OFF"),
+            ),
+            ElevatedButton(
+              onPressed: () => send("add"),
+              child: Text("Add Text"),
+            ),
+            Expanded(child: SingleChildScrollView(child: Text(text))),
+          ],
+        ),
+      ),
     );
   }
 }
